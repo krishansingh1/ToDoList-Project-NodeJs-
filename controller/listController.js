58144e6f7a3a457af9bfe6b8c23c690c3ed97bb0
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-26 14:46:58 +0530

    Commit

---
+++ controller/listController.js
@@ -0,0 +1,3 @@
+module.exports.list = function (res, req) {
+  return res.end("<h1>ToDo App</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+const listController = require("../controller/listController");
+
+router.get("/", listController.list);
+
 module.exports = router;
