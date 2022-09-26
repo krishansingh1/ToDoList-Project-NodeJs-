5e236a45b761701318cf275669d78735180a9c64
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-26 16:37:45 +0530

    Commit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 8000;
 //Use express router
 app.use("/", require("./routes"));
 
+app.set("view engine", "ejs");
+
+app.set("views", "./views");
+
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error: ${err}`);
