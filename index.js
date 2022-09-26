e0e13dc3aa630b34fbbce4512f1cfd21212c2787
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-26 14:28:18 +0530

    Commit

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 
 const port = 8000;
 
+//Use express router
+app.use("/", require("./routes"));
+
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error: ${err}`);
   }
-  console.log(`Successfully setup the app and runnin on port: ${port}`);
+  console.log(`Server is successfully running on port: ${port}`);
 });
