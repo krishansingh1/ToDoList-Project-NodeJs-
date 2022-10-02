88a943179a282f734b3ae3240af11a2691935817
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-02 19:32:38 +0530

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const { application } = require("express");
 const express = require("express");
-
 const app = express();
-
 const port = 8000;
+const bodyParser = require("body-parser");
 
 //Use express router
 app.use("/", require("./routes"));
@@ -12,6 +12,10 @@ app.use(express.static("assests"));
 app.set("view engine", "ejs");
 
 app.set("views", "./views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {});
 
 app.listen(port, (err) => {
   if (err) {
