d53190fbd711e849d965aef444dd72b3a335261f
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-02 22:45:48 +0530

    Commit

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ app.post("/create-list", function (req, res) {
 });
 
 app.get("/delete-list", function (req, res) {
-  console.log(req.query);
+  console.log(req.query.description);
+
+  return res.redirect("/");
 });
 
 app.listen(port, (err) => {
