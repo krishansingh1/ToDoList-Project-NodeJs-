0d0e4fb97f2864fef93f216def69f70e2e79101f
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-08 17:08:00 +0530

    Commit

--- assests/js/script.js
+++ assests/js/script.js
@@ -17,19 +17,6 @@ btn.addEventListener("click", function () {
   window.location = `http://localhost:8000/delete-list/?id=${str}`;
 });
 
-// console.log(listOptions);
-// console.log(listOptions.options);
-// console.log(color);
-
-// if (listOptions.options[0]) {
-//   color.classList.add("background_color1");
-// }
-
-// if (listOptions.options[1]) {
-//   color.classList.remove("background_color1");
-//   color.classList.add("background_color2");
-// }
-
-// if (listOptions.options[2]) {
-//   color.style.backgroundColor = "violet";
-// }
+if (listOptions.options[0]) {
+  
+}
