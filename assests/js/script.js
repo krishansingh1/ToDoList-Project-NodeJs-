b157c76c52eb33e1d9ed735224037662d81c489d
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-08 16:47:50 +0530

    Commit

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,6 @@
 let btn = document.getElementById("list_delete2");
+let listOptions = document.querySelector("select");
+let color = document.getElementById("list_content_category");
 let str = "";
 
 btn.addEventListener("click", function () {
@@ -14,3 +16,20 @@ btn.addEventListener("click", function () {
 
   window.location = `http://localhost:8000/delete-list/?id=${str}`;
 });
+
+// console.log(listOptions);
+// console.log(listOptions.options);
+// console.log(color);
+
+// if (listOptions.options[0]) {
+//   color.classList.add("background_color1");
+// }
+
+// if (listOptions.options[1]) {
+//   color.classList.remove("background_color1");
+//   color.classList.add("background_color2");
+// }
+
+// if (listOptions.options[2]) {
+//   color.style.backgroundColor = "violet";
+// }
